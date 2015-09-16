44a99d3b8d85f3e460c74f6c0acb4583b92cfe7e
caoyh188 <caoyh@mapbar.com>
2015-09-16 15:55:01 +0800

    commit

---
+++ gitTest/src/com/git/page/gitTest.java
@@ -0,0 +1,24 @@
+/** 
+ * <pre>项目名称:gitTest 
+ * 文件名称:gitTest.java 
+ * 包名:com.git.page 
+ * 创建日期:2015年9月16日下午3:54:12 
+ * Copyright (c) 2015,  All Rights Reserved.</pre> 
+ */  
+package com.git.page;
+
+/** 
+ * <pre>项目名称：gitTest    
+ * 类名称：gitTest    
+ * 类描述：    
+ * 创建人：caoyh  
+ * 创建时间：2015年9月16日 下午3:54:12    
+ * 修改备注：       
+ * @version </pre>    
+ */
+public class gitTest {
+
+	public static void main(String[] args) {
+		System.out.println("aa");
+	}
+}
